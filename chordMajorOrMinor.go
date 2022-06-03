7ddfa5e40307341ba28ca8ac232e2fbedbc5e1c2
Aelish <alecrollison@icloud.com>
2022-06-03 00:40:23 -0400

    Create chordMajorOrMinor file. Has basic logic used for recoginizing major and minor thirds

---
+++ chordMajorOrMinor.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("Hello, 世界")
+
+        A := 10
+        B := 13
+
+        if B-A == 3 {
+                fmt.Println("Minor Chord")
+        } else if B-A == 4 {
+                fmt.Println("Minor Chord")
+        }
+}
